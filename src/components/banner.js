8a5a0b47fade03060d28db7ae52ed0ddfd0a82ff
Li <liyunwei10@gmail.com>
2020-08-21 21:24:38 -0400

    add banner

---
+++ src/components/banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactComponent as RightArrow } from "../assets/images/right-arrow.svg";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <p className="bio">
+        Hi there <div className="wave">👋</div> My name is Li. I'm a full stack
+        developer. I'm also interested in AI and Game Development.
+      </p>
+
+      <div className="about">
+        <a>
+          <p>More about me</p>
+          <RightArrow className="right-arrow" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
